c72d887d14c813aec4baacf864fc8ed902b94298
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2019-10-03 19:36:22 -0400

    Added ravenous build version to completed directory

---
+++ Completed/Ravenous/precache-manifest.da398a35e96e61d2ccbb1d403604d095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Completed/Ravenous/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Completed/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Completed/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "83c7507b1cf635558448",
+    "url": "/Completed/Ravenous/static/js/runtime~main.c025ee79.js"
+  },
+  {
+    "revision": "5ebebb57c9ed59ac7747",
+    "url": "/Completed/Ravenous/static/js/main.1cc412db.chunk.js"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/Completed/Ravenous/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "5ebebb57c9ed59ac7747",
+    "url": "/Completed/Ravenous/static/css/main.4c35b43d.chunk.css"
+  },
+  {
+    "revision": "f770eeb2c57d539664308b3174df2664",
+    "url": "/Completed/Ravenous/index.html"
+  }
+];
